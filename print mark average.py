a26c4512c98a1fca6db7630271d01667e628879a
Pratik Hake <158798829+HakePratik@users.noreply.github.com>
2024-08-18 11:37:06 +0530

    Update print mark average.py

--- print mark average.py
+++ print mark average.py
@@ -1,7 +1,8 @@
 n = int(input("enter no of students:"))
 student_marks = {}
+print ("first write name space write marks.\n")
 for i in range(n):
-    name, *line = input("First write name space write marks.\n).split()
+    name, *line = input().split()
     scores = list(map(float, line))
     student_marks[name] = scores
 query_name = input("student name:")
@@ -9,3 +10,4 @@ l1 = list(student_marks[query_name])
 addition = sum(l1)
 result = addition/len(l1)
 print('%.2f'% result)
+#2f use for flot result print 
